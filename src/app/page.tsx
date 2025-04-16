a335a4c5c4180edade187db409d029be5b37cc52
RedHolms <arseny.saw4enko@gmail.com>
2025-04-16 19:16:26 +0300

    got it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   return (
     <div className={"w-screen h-screen flex justify-center items-center bg-[#4d3957] select-none " + styles.background}>
       <div className={"w-150 h-90 bg-[#242424] flex flex-col items-center relative text-lg " + styles.box}>
-        <h1 className="text-xl mt-7">Here's some of my links:</h1>
+        <h1 className="text-xl mt-7">Here&apos;s some of my links:</h1>
         
         <MyLink className="mt-2" href="https://github.com/RedHolms">GitHub</MyLink>
         <MyLink href="https://t.me/redholms">Telegram</MyLink>
